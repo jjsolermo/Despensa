7375e580d403ef8a92ad686f7af9990091ae8ad6
Juanjo Soler <jjsolermo@hotmail.com>
2023-08-04 12:04:10 +0200

    Primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  usuario = '';
+  password = '';
+  constructor(private auth: Auth, private router: Router, private alertController: AlertController) { }
 
-  constructor() {}
+  async login() {
+    try {
+      const user = await signInWithEmailAndPassword(this.auth, this.usuario, this.password);
+      if (user) {
+        this.router.navigateByUrl('/main', { replaceUrl: true });
+      }
+    } catch (e) {
+      this.presentAlert();
+      console.error(e);
+    }
+  }
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Algo a courrido',
+      message: 'Usuario o contraseña incorrectos',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'despensa-671f5',
+    appId: '1:31126648512:web:30cd026e46833e56829f25',
+    storageBucket: 'despensa-671f5.appspot.com',
+    apiKey: 'AIzaSyDjp12Y7iED85--m0gx1hWheg1PDl9UtYw',
+    authDomain: 'despensa-671f5.firebaseapp.com',
+    messagingSenderId: '31126648512',
+    measurementId: 'G-FM1BQK8X71',
+  },
   production: true
 };
